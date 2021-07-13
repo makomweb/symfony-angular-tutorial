7e3463e1f1cd374206857ba8733f735c3a0b64a4
Martin Komischke <martin.komischke@gmail.com>
2021-07-13 16:10:39 +0200

    implement weather service

--- client/src/app/weather.service.ts
+++ client/src/app/weather.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export interface Weather
+{
+  temperature: Number,
+  sky: String
+}
+
+export interface WeatherForecast
+{
+  forecast: Weather[]
+}
+
+const API_URL: string = 'http://localhost:8000';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getToday() {
+    return this.http.get<Weather>(API_URL + '/today');
+  }
+
+  getForecast() {
+    return this.http.get<WeatherForecast>(API_URL + '/forecast');
+  }
 }
