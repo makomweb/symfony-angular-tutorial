d353e032317f43d1a14c57216185bae323e9ad80
Martin Komischke <martin.komischke@gmail.com>
2021-07-14 09:51:13 +0200

    add Material toolbar

--- client/src/app/material.module.ts
+++ client/src/app/material.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({ 
     exports: [ 
         MatButtonModule,
         MatIconModule,
-        MatCardModule
+        MatCardModule,
+        MatToolbarModule
     ]})
 
 export class MaterialModule { }
